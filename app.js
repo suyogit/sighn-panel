a461d18dac9cdcd10857193c7f3ec3e496923a87
Suyog Acharya <asuyog042@gmail.com>
2023-03-17 19:38:55 +0545

    site running on localhost

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 //jshint esversion: 6
 
-const express =require("express");
-const bodyParser=require("body-parser");
-const request=require('request');
-const app= express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.get("/", function(req,res)
-{
- res.sendFile(__dirname+"/signup.html");
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req,res)
-{
+app.post("/", function (req, res) {});
 
-});
-
-
-app.listen(3000, function()
-{console.log("Server started on port3000");
+app.listen(3000, function () {
+  console.log("Server started on port3000");
 });
