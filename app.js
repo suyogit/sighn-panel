c4360a1384c9388010d51d7eadbfbc7c05c0f056
Suyog Acharya <asuyog042@gmail.com>
2023-03-17 19:52:12 +0545

    a boilerplate for creating signup pannel

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function (req, res) {});
+app.post("/", function (req, res) {
+  var firstname = req.body.fname;
+  var lastname = req.body.lname;
+  var email = req.body.email;
+
+  console.log(firstname, lastname, email);
+});
 
 app.listen(3000, function () {
   console.log("Server started on port3000");
