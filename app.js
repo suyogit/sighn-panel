2824a28978dde7e297aa3724f4675780d9cdf0b0
Suyog Acharya <asuyog042@gmail.com>
2023-03-20 07:59:06 +0545

    final commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //jshint esversion: 6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("node:https");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -13,13 +14,50 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var firstname = req.body.fname;
-  var lastname = req.body.lname;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstname, lastname, email);
+  // console.log(firstname, lastname, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/list_id_here";
+
+  const options = {
+    method: "POST",
+    auth: "suyogit:api_keyhere",
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
 
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
 app.listen(3000, function () {
   console.log("Server started on port3000");
 });
